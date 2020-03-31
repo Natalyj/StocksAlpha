c0982dada81d1828afe82465a853ff86275dd5c6
Natalia Tokmakova <natalyj100@gmail.com>
2020-03-31 22:43:32 +0300

    centralized getting of time series in one file

--- src/alphaApi/getTimeSeries.ts
+++ src/alphaApi/getTimeSeries.ts
@@ -1,6 +1,12 @@
 import { StockInfo, TimeSeries, Interval, PriceField } from './types';
-import { requestTimeSeries } from './requestTimeSeries';
-import { parseTimeSeries } from './parseTimeSeries';
+import {
+  isIntervalFunction,
+  requestJson,
+  getTimeZone,
+  getCurrentDate,
+  getFieldValue,
+} from './utils';
+import { QUERY_START, APIKEY_QUERY } from './constants';
 
 export const getTimeSeries = async (
   timeSeries: TimeSeries,
@@ -11,3 +17,36 @@ export const getTimeSeries = async (
   await requestTimeSeries(timeSeries, company, interval).then(series =>
     parseTimeSeries(series, priceField)
   );
+
+const requestTimeSeries = async (
+  timeSeries: TimeSeries,
+  company: string,
+  interval: Interval
+): Promise<any> => {
+  const intervalQuery = isIntervalFunction(timeSeries)
+    ? `&interval=${interval}`
+    : '';
+  const requestQuery = `${QUERY_START}function=${timeSeries}&symbol=${company}${intervalQuery}&${APIKEY_QUERY}`;
+
+  return await requestJson(requestQuery);
+};
+
+const parseTimeSeries = (
+  responseJson: any,
+  priceField: PriceField
+): Array<StockInfo> => {
+  const entries: [string, any][] = Object.entries(responseJson);
+  const timeZone = getTimeZone(entries[0][1]);
+  const timeSeriesEntries: [string, any][] = Object.entries(entries[1][1]);
+
+  const prices: Array<StockInfo> = [];
+
+  for (const [date, values] of timeSeriesEntries) {
+    prices.push({
+      date: getCurrentDate(date, timeZone),
+      price: Number(getFieldValue(values, priceField)),
+    });
+  }
+
+  return prices;
+};
